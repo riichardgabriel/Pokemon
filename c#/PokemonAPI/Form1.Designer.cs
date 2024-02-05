f267eada14820876972e4661adaf0b04166b762d
riichardgabriel <141831980+riichardgabriel@users.noreply.github.com>
2024-02-05 11:59:49 -0300

    sla
    comentário

---
+++ c#/PokemonAPI/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace PokemonAPI
+{
+    partial class Pokedex
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConsultaPokemon = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtNomePokemon = new System.Windows.Forms.TextBox();
+            this.lbID = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbTipo = new System.Windows.Forms.Label();
+            this.lbPeso = new System.Windows.Forms.Label();
+            this.lbAltura = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConsultaPokemon
+            // 
+            this.btnConsultaPokemon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnConsultaPokemon.Location = new System.Drawing.Point(185, 400);
+            this.btnConsultaPokemon.Name = "btnConsultaPokemon";
+            this.btnConsultaPokemon.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultaPokemon.TabIndex = 0;
+            this.btnConsultaPokemon.Text = "Consultar";
+            this.btnConsultaPokemon.UseVisualStyleBackColor = true;
+            this.btnConsultaPokemon.Click += new System.EventHandler(this.btnConsultaPokemon_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 270);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtNomePokemon
+            // 
+            this.txtNomePokemon.Location = new System.Drawing.Point(12, 402);
+            this.txtNomePokemon.Name = "txtNomePokemon";
+            this.txtNomePokemon.Size = new System.Drawing.Size(153, 20);
+            this.txtNomePokemon.TabIndex = 2;
+            // 
+            // lbID
+            // 
+            this.lbID.AutoSize = true;
+            this.lbID.Location = new System.Drawing.Point(12, 301);
+            this.lbID.Name = "lbID";
+            this.lbID.Size = new System.Drawing.Size(18, 13);
+            this.lbID.TabIndex = 3;
+            this.lbID.Text = "ID";
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(12, 314);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(35, 13);
+            this.lbName.TabIndex = 4;
+            this.lbName.Text = "Nome";
+            // 
+            // lbTipo
+            // 
+            this.lbTipo.AutoSize = true;
+            this.lbTipo.Location = new System.Drawing.Point(12, 327);
+            this.lbTipo.Name = "lbTipo";
+            this.lbTipo.Size = new System.Drawing.Size(28, 13);
+            this.lbTipo.TabIndex = 5;
+            this.lbTipo.Text = "Tipo";
+            // 
+            // lbPeso
+            // 
+            this.lbPeso.AutoSize = true;
+            this.lbPeso.Location = new System.Drawing.Point(12, 340);
+            this.lbPeso.Name = "lbPeso";
+            this.lbPeso.Size = new System.Drawing.Size(31, 13);
+            this.lbPeso.TabIndex = 6;
+            this.lbPeso.Text = "Peso";
+            // 
+            // lbAltura
+            // 
+            this.lbAltura.AutoSize = true;
+            this.lbAltura.Location = new System.Drawing.Point(12, 353);
+            this.lbAltura.Name = "lbAltura";
+            this.lbAltura.Size = new System.Drawing.Size(34, 13);
+            this.lbAltura.TabIndex = 7;
+            this.lbAltura.Text = "Altura";
+            // 
+            // Pokedex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(294, 450);
+            this.Controls.Add(this.lbAltura);
+            this.Controls.Add(this.lbPeso);
+            this.Controls.Add(this.lbTipo);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.lbID);
+            this.Controls.Add(this.txtNomePokemon);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnConsultaPokemon);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Pokedex";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pokedex do perigo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConsultaPokemon;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtNomePokemon;
+        private System.Windows.Forms.Label lbID;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbTipo;
+        private System.Windows.Forms.Label lbPeso;
+        private System.Windows.Forms.Label lbAltura;
+    }
+}
+
---
+++ c#/PokemonAPI/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PokemonAPI
+{
+    public partial class Pokedex : Form
+    {
+        private const string ApiBaseUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+        public Pokedex()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnConsultaPokemon_Click(object sender, EventArgs e)
+        {
+            string pokemonName = txtNomePokemon.Text.Trim();
+
+            if (!string.IsNullOrEmpty(pokemonName))
+            {
+                try
+                {
+                    string apiUrl = $"{ApiBaseUrl}{pokemonName.ToLower()}/";
+                    PokemonData pokemonData = await GetPokemonData(apiUrl);
+
+                    if (pokemonData != null)
+                    {
+                        DisplayPokemonInfo(pokemonData);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Pokémon não encontrado.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao consultar a API: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Digite o nome de um Pokémon.");
+            }
+        }
+
+        private void DisplayPokemonInfo(PokemonData pokemonData)
+        {
+            // Exibir informações do Pokémon
+            lbID.Text = $"ID: {pokemonData.id}";
+            lbName.Text = $"Nome: {pokemonData.name}";
+            lbTipo.Text = $"Tipo: {string.Join(", ", pokemonData.types.Select(t => t.type.name))}";
+            lbPeso.Text = $"Peso: {pokemonData.weight} kg";
+            lbAltura.Text = $"Altura: {pokemonData.height} m";
+
+            // Exibir imagem do Pokémon
+            DisplayPokemonImage(pokemonData.sprites.front_default);
+        }
+
+        private async Task<PokemonData> GetPokemonData(string apiUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    //instale atravez do NuGet Newtonsoft.Json
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<PokemonData>(jsonResponse);
+                }
+            }
+
+            return null;
+        }
+
+        private void DisplayPokemonImage(string imageUrl)
+        {
+            if (!string.IsNullOrEmpty(imageUrl))
+            {
+                pictureBox1.Size = new Size(300, 300);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load(imageUrl);
+            }
+        }
+
+
+        public class PokemonData
+        {
+            public string name { get; set; }
+            public int id { get; set; }
+            public List<TypeData> types { get; set; }
+            public float weight { get; set; }
+            public float height { get; set; }
+            public Sprites sprites { get; set; }
+        }
+
+        public class TypeData
+        {
+            public Type type { get; set; }
+        }
+
+        public class Type
+        {
+            public string name { get; set; }
+        }
+
+        public class Sprites
+        {
+            public string front_default { get; set; }
+        }
+    }
+
+}
